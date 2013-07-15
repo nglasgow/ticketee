ab49277874fa94d4b5d0d5103cefeeeffd393cdd
Nicle Glasgow <nicole.glasgow@ymail.com>
2013-07-15 13:21:12 -0700

    Redirect the users back to the projects page if they try going to a project that doesn't exist.

--- spec/integration/creating_projects_spec.rb
+++ spec/integration/creating_projects_spec.rb
@@ -5,6 +5,7 @@ feature 'Creating Projects' do
     visit '/'
     click_link 'New Project'
   end
+
   scenario "can create a project" do
     fill_in 'Name', :with => 'Textmate 2'
     fill_in 'Description', :with => "A text-editor for OS X"
@@ -15,6 +16,7 @@ feature 'Creating Projects' do
     title = "Textmate 2 - Projects - Ticketee"
     find("title").should have_content(title)
   end
+
   scenario "can not create a project without a name" do
     click_button 'Create Project'
     page.should have_content("Project has not been created.")
