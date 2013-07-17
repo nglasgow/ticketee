6050a96b5a10ed879e24f1ca3d3782e120222d9b
nglasgow <nicole.glasgow@ymail.com>
2013-07-17 14:40:04 -0700

    Add feature to ensure Devise signup is always working

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,5 @@
 Ticketee::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
-
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
@@ -34,4 +33,7 @@ Ticketee::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
 end
