4eb224721639bed61aae4172ac0a6b698cf72407
nglasgow <nicole.glasgow@ymail.com>
2013-07-18 16:46:38 -0700

    Restrict access to project actions to admins only

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 Ticketee::Application.routes.draw do
+  namespace :admin do
+    root :to => "base#index"
+    resources :users
+  end
+
   devise_for :users
 
-  root to: "projects#index"
+  root :to => "projects#index"
+
   resources :projects do
     resources :tickets
   end
+
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
