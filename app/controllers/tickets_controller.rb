1b9d47f8784ff2cb42eebd39a91bcf21cca302dd
Nicole Glasgow <nicole.glasgow@ymail.com>
2013-07-25 21:38:39 -0700

    Restrict creating tickets to only users who have permissions to do it

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -2,6 +2,7 @@ class TicketsController < ApplicationController
 before_filter :authenticate_user!
 before_filter :find_project
 before_filter :find_ticket, :only => [:show, :edit, :update, :destroy]
+before_filter :authorize_create!, :only => [:new, :create]
 
 def find_ticket
   @ticket = @project.tickets.find(params[:id])
@@ -54,5 +55,12 @@ private
       flash[:alert] = "The project you were looking " +
                       "for could not be found."
         redirect_to root_path
+  end
+
+  def authorize_create!
+    if !current_user.admin? && cannot?("create tickets".to_sym, @project)
+      flash[:alert] = "You cannot create tickets on this project."
+      redirect_to @project
+    end
   end
 end
--- spec/controllers/tickets_controller_spec.rb
+++ spec/controllers/tickets_controller_spec.rb
@@ -8,11 +8,34 @@ describe TicketsController do
 
   context "standard users" do
     it "cannot access a ticket for a project" do
-    sign_in(:user, user)
-    get :show, :id => ticket.id, :project_id => project.id
-    response.should redirect_to(root_path)
-    flash[:alert].should eql("The project you were looking " +
-                              "for could not be found.")
+      sign_in(:user, user)
+      get :show, :id => ticket.id, :project_id => project.id
+      response.should redirect_to(root_path)
+      flash[:alert].should eql("The project you were looking " +
+                                "for could not be found.")
+    end
+  end
+
+  context "with permission to view the project" do
+    before do
+      sign_in(:user, user)
+      define_permission!(user, "view", project)
+    end
+
+    def cannot_create_tickets!
+      response.should redirect_to(project)
+      message = "You cannot create tickets on this project."
+      flash[:alert].should eql(message)
+    end
+
+    it "cannot begin to create a ticket" do
+      get :new, :project_id => project.id
+      cannot_create_tickets!
+    end
+
+    it "cannot create a ticket without permission" do
+      post :create, :project_id => project.id
+      cannot_create_tickets!
     end
   end
 end
